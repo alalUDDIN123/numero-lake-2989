d272b61ecc8e59891b3781fb2b46050872e7adba
Alal Uddin <alalu75666@gmail.com>
2022-12-14 08:46:30 +0530

    Add styles folder and install styled_components

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import styles from "../styles/Admin/Main.module.css"
 function Admin() {
   return (
     <div>
-      Admin
+     <section>
+       {/* Left side */}
+        <div className={styles.max_width}>
+          <div></div>
+       
+       
+
+
+       {/* Right side */}
+       <div></div>
+       </div>
+     </section>
     </div>
   )
 }
