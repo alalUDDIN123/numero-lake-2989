77b1c0d8256abd386a2897826f611ec9da5a0502
Alal Uddin <alalu75666@gmail.com>
2022-12-16 17:21:56 +0530

    updated

--- src/Pages/Admin/Top.js
+++ src/Pages/Admin/Top.js
@@ -33,26 +33,6 @@ function Top({getterm}) {
                     <img src="https://static.vecteezy.com/system/resources/previews/001/591/517/non_2x/free-search-icon-free-vector.jpg" alt=""
                    />
                 </div>
-                {/* <div className={styles.category}>
-                    <h1>Category : </h1>
-                    <select name="" >
-                        <option value="">Select category</option>
-                        <option value="bike">Bike</option>
-                        <option value="mob">Mobile</option>
-                    </select>
-
-
-                </div>
-                <div className={styles.category}>
-                    <h1>Sort By: </h1>
-                    <select name="" >
-                        <option value="">Sort By</option>
-                        <option value="high">High-to-low</option>
-                        <option value="low">Low-to-high</option>
-                    </select>
-
-
-                </div> */}
             </div>
         </>
     )
