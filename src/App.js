73ed054b251b37f070e5aa873af132359e5f056b
setu bhaskar jha <sbhaskar1998@gmail.com>
2022-12-16 17:49:45 +0530

    resolved some errors in webpack config file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import AllRoutes from "./Componets/AllRoutes";
 import { AuthTesting } from "./Pages/AuthTesting";
+import { ProductDetails } from "./Pages/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-      <AllRoutes />
+      {/* <AllRoutes /> */}
+      <ProductDetails />
     </div>
   );
 }
