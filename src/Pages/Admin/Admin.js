b73094fecf0121dac07ae7d30dd9a35daaf18190
setu bhaskar jha <sbhaskar1998@gmail.com>
2022-12-16 18:20:47 +0530

    resolved merge conflicts

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,23 +1,17 @@
-<<<<<<< HEAD
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "../../styles/Admin/Main.module.css";
-=======
-import React, { useEffect} from 'react'
-import styles from "../../styles/Admin/Main.module.css"
-import sidebar from "../../styles/Admin/sidebar.module.css"
-import Top from './Top'
-import { Right } from "./Right"
+import sidebar from "../../styles/Admin/sidebar.module.css";
+import Top from "./Top";
+import { Right } from "./Right";
 
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { getProducts } from '../../Redux/actions'
->>>>>>> 47bfafe3437d41110390e813a2adde26f05e9a12
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../../Redux/actions";
 function Admin() {
-  const data= useSelector(store=>store.data)
+  const data = useSelector((store) => store.data);
 
-  const naviagte=useNavigate()
-  const dispatch= useDispatch()
-
+  const naviagte = useNavigate();
+  const dispatch = useDispatch();
 
   // useEffect(() => {
   //   if (window.innerWidth === 1366 || window.innerWidth > 1200) {
@@ -25,45 +19,30 @@ function Admin() {
   //   }
   // }, [])
 
-
-
   // add products button redirect
-  const addproducts=()=>{
-    naviagte("addProduct")
-  }
-  // admin/users 
+  const addproducts = () => {
+    naviagte("addProduct");
+  };
+  // admin/users
 
-  const UsersClick=()=>{
-    naviagte("users")
-  }
+  const UsersClick = () => {
+    naviagte("users");
+  };
 
-   // admin/contact
-   const contactClick=()=>{
-    naviagte("contact")
-  }
+  // admin/contact
+  const contactClick = () => {
+    naviagte("contact");
+  };
 
-
- useEffect(()=>{
-  if(data.length===0){
-    dispatch(getProducts())
-  }
- },[dispatch,data.length])
-
-
-
+  useEffect(() => {
+    if (data.length === 0) {
+      dispatch(getProducts());
+    }
+  }, [dispatch, data.length]);
 
   return (
     <div>
-<<<<<<< HEAD
-      <section>
-        {/* Left side */}
-        <div className={styles.max_width}>
-          <div></div>
-
-          {/* Right side */}
-          <div></div>
-=======
-      <Top  />
+      <Top />
       <section>
         {/* Left side */}
         <div className={styles.max_width}>
@@ -73,42 +52,37 @@ function Admin() {
             <h1>All products</h1>
             <h1 onClick={UsersClick}>Users</h1>
             <h1 onClick={contactClick}>Contact</h1>
-          
-           
           </div>
 
           {/* Right side */}
-          
-    <div className={sidebar.right}>
-    <table>
-      <thead>
-        <tr>
-          <th>S.No</th>
-          <th>Images</th>
-          <th>Title</th>
-          <th>Price</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
 
-      <tbody>
-        {data && data.map((item, index) => (
-          <Right
-            index={index + 1}
-            productId={item.id}
-            key={item.id}
-            title={item.title}
-            image={item.image}
-            price={item.price}
+          <div className={sidebar.right}>
+            <table>
+              <thead>
+                <tr>
+                  <th>S.No</th>
+                  <th>Images</th>
+                  <th>Title</th>
+                  <th>Price</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
 
-          />
-        ))}
-      </tbody>
-    </table>
-  </div>
-
-           
->>>>>>> 47bfafe3437d41110390e813a2adde26f05e9a12
+              <tbody>
+                {data &&
+                  data.map((item, index) => (
+                    <Right
+                      index={index + 1}
+                      productId={item.id}
+                      key={item.id}
+                      title={item.title}
+                      image={item.image}
+                      price={item.price}
+                    />
+                  ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </section>
     </div>
