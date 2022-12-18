b74dbec9250c164cc49d2c41da4e100d6b15059f
sanghamitra0591 <sanghamitramymail@gmail.com>
2022-12-19 00:51:06 +0530

    added

--- src/Pages/AuthTesting.jsx
+++ src/Pages/AuthTesting.jsx
@@ -1,29 +1,29 @@
-import { Button } from "@chakra-ui/react";
-import { useDispatch } from "react-redux";
-import { signInWithGoogle } from "../Redux/AuthReducer/action";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { firebaseApp } from "../Services/firebase";
+// import { Button } from "@chakra-ui/react";
+// import { useDispatch } from "react-redux";
+// import { signInWithGoogle } from "../Redux/AuthReducer/action";
+// import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// import { firebaseApp } from "../Services/firebase";
 
-const provider = new GoogleAuthProvider();
+// const provider = new GoogleAuthProvider();
 
-const auth = getAuth(firebaseApp);
+// const auth = getAuth(firebaseApp);
 
-export const AuthTesting = () => {
-  const dispatch = useDispatch();
+// export const AuthTesting = () => {
+//   const dispatch = useDispatch();
 
-  const handleClick = () => {
-    dispatch(signInWithGoogle(auth, provider));
-  };
+//   const handleClick = () => {
+//     dispatch(signInWithGoogle(auth, provider));
+//   };
 
-  return (
-    <Button colorScheme="blue" onClick={handleClick}>
-      Continue with Google
-    </Button>
-  );
-};
+//   return (
+//     <Button colorScheme="blue" onClick={handleClick}>
+//       Continue with Google
+//     </Button>
+//   );
+// };
 
-/* 
-displayName
-email
-photoURL
-*/
+// /* 
+// displayName
+// email
+// photoURL
+// */
