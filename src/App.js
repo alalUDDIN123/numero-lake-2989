543efa0a545c4c7d21a7c5381c1bade5409db4e1
setu bhaskar jha <sbhaskar1998@gmail.com>
2022-12-18 18:45:13 +0530

    added: product details page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import AllRoutes from "./Componets/AllRoutes";
 import { AuthTesting } from "./Pages/AuthTesting";
-import { ProductDetails } from "./Pages/ProductDetails";
 
 function App() {
   return (
